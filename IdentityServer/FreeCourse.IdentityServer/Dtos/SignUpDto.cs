64d012b4d2d543a7c5c9378ad5011b9dd1496d06
ataberk <ataberk.kurumoglu@d1-tech.com>
2023-04-25 13:34:29 +0300

    add auth identityserver to catalog api

---
+++ IdentityServer/FreeCourse.IdentityServer/Dtos/SignUpDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.IdentityServer.Dtos
+{
+    public class SignUpDto
+    {
+        [Required]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
